62339c6b54bc486682abd7bd8dd2481dcedacec2
aldringalera12 <aldringalera12@gmail.com>
2025-03-01 00:56:42 +0800

    try

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: ["next/core-web-vitals"],
-    rules: {
-      // Add custom rules here
-      "jsx-a11y/alt-text": "warn", // Warn about missing alt attributes
-      "react-hooks/exhaustive-deps": "warn", // Warn about missing useEffect dependencies
-      "react/no-unescaped-entities": "warn", // Warn about unescaped entities
-    },
-  };
